5d8dd4f136b2e89b02bad8e0b8d9cabb27648ba6
Wanfung Lee <cloudwindhk@gmail.com>
2016-03-11 16:02:33 -0800

    much more efficient matching

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 var elements = document.getElementsByTagName('*');
 
+function toTitleCase(str){
+    return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();
+}
+
 var wordsToReplace = [
     ['up', 'down'],
     ['down', 'up'],
@@ -8,15 +12,7 @@ var wordsToReplace = [
     ['north', 'south'],
     ['south', 'north'],
     ['east', 'west'],
-    ['west', 'east'],
-    ['Up', 'Down'],
-    ['Down', 'Up'],
-    ['Left', 'Right'],
-    ['Right', 'Left'],
-    ['North', 'South'],
-    ['South', 'North'],
-    ['East', 'West'],
-    ['West', 'East']
+    ['west', 'east']
 ];
 
 for(var i = 0; i < elements.length; i++){
@@ -33,16 +29,14 @@ for(var i = 0; i < elements.length; i++){
                 for(var k = 0; k < wordsToReplace.length; k++){
                     var wordToReplace = wordsToReplace[k];
                     globalSearchString += wordToReplace[0] + '|';
+                    globalSearchString += toTitleCase(wordToReplace[0]) + '|';
                 }
-                replacedText = replacedText
-                    .replace(new RegExp('([^A-Za-z])+(' + globalSearchString.substring(0, globalSearchString.length - 1) + ')([^A-Za-z])+', 'g'), '$1WAIT$2WAIT$3')
-                    .replace(new RegExp('^(' + globalSearchString.substring(0, globalSearchString.length - 1) + ')([^A-Za-z])+', 'g'), 'WAIT$1WAIT$2')
-                    .replace(new RegExp('([^A-Za-z])+(' + globalSearchString.substring(0, globalSearchString.length - 1) + ')$', 'g'), '$1WAIT$2WAIT')
-                    .replace(new RegExp('^(' + globalSearchString.substring(0, globalSearchString.length - 1) + ')$', 'g'), 'WAIT$1WAIT');
+                replacedText = replacedText.replace(new RegExp('\\b(' + globalSearchString.substring(0, globalSearchString.length - 1) + ')\\b', 'g'), 'WAIT$1WAIT');
                 for(var k = 0; k < wordsToReplace.length; k++){
                     var wordToReplace = wordsToReplace[k];
                     replacedText = replacedText
-                        .replace(new RegExp('WAIT' + wordToReplace[0] + 'WAIT', 'g'), wordToReplace[1]);
+                        .replace(new RegExp('WAIT' + wordToReplace[0] + 'WAIT', 'g'), wordToReplace[1])
+                        .replace(new RegExp('WAIT' + toTitleCase(wordToReplace[0]) + 'WAIT', 'g'), toTitleCase(wordToReplace[1]));
                 }
                 if(replacedText !== text){
                     element.replaceChild(document.createTextNode(replacedText), node);
